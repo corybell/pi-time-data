60d2d451a0a2d1b3cf907b1b1e0a53f17a70c091
Cory Bell <corybell21@gmail.com>
2021-07-12 09:29:56 -0500

    load env into container, health check

--- api/__init__.py
+++ api/__init__.py
@@ -1,14 +1,19 @@
+from dependency_injector import providers
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
 from .containers import Container
 from .blueprints import health_check, relay, timer
 
-load_dotenv()
-
 def create_app() -> Flask:
+    load_dotenv()
+
     container = Container()
-    container.wire(modules=[relay, timer])
+    container.config = providers.Configuration()
+    container.config.host.from_env('API_HOST')
+    container.config.port.from_env('API_PORT')
+
+    container.wire(modules=[health_check, relay, timer])
     
     app = Flask(__name__)
     app.container = container
@@ -17,4 +22,5 @@ def create_app() -> Flask:
     app.register_blueprint(health_check.blueprint)
     app.register_blueprint(timer.blueprint)
     app.register_blueprint(relay.blueprint)
+    
     return app
--- api/blueprints/health_check.py
+++ api/blueprints/health_check.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, jsonify
+from dependency_injector.providers import Configuration
+from dependency_injector.wiring import inject, Provide
+from ..containers import Container
 
 blueprint = Blueprint('health_check', __name__)
 
 @blueprint.route('/health-check', methods=['GET'])
-def health_check():
-  return jsonify(version='1.0.0'), 200
+@inject
+def health_check(config: Configuration = Provide[Container.config]):
+  return jsonify(version='1.0.0', config=config), 200
