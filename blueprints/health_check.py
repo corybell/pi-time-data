0915b6540f22c683186bf58d8db3d4417fc136c2
Cory Bell <corybell21@gmail.com>
2021-07-13 19:18:47 -0500

    return version in health check

--- blueprints/health_check.py
+++ blueprints/health_check.py
@@ -2,10 +2,11 @@ from flask import Blueprint, jsonify
 from dependency_injector.providers import Configuration
 from dependency_injector.wiring import inject, Provide
 from app.container import AppContainer
+from app.version import version
 
 blueprint = Blueprint('health_check', __name__)
 
 @blueprint.route('/health-check', methods=['GET'])
 @inject
 def health_check(config: Configuration = Provide[AppContainer.config]):
-  return jsonify(version='1.0.0', config=config), 200
+  return jsonify(version=version, config=config), 200
