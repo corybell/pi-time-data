5a806a15047b67cbc1e4b1d88dbb3c7b22b6cbef
Cory Bell <corybell21@gmail.com>
2021-07-14 17:42:30 -0500

    seed relay 1 with example data

--- seed.py
+++ seed.py
@@ -1,6 +1,23 @@
 from util import write_file
 from sys import argv
 
+def build_relay(r: int):
+  if r == 0:
+    return {
+      'id': '1',
+      'name': 'Primary Lights',
+      'timer': {
+        'hr': 'eighteen',
+        'min': '*'
+      }
+    }
+
+  return {
+    'id': str(r + 1),
+    'name': '',
+    'timer': None
+  }
+
 def get_input():
   if (len(argv) != 2):
     print('Usage: ./scripts/seed.sh <NUM_RELAYS>')
@@ -11,11 +28,7 @@ num_relays = get_input()
 
 relay_data = []
 for r in range(0, num_relays):
-  relay_data.append({
-  'id': str(r + 1),
-  'name': '',
-  'timer': None
-})
+  relay_data.append(build_relay(r))
 
 if len(relay_data):
   write_file('data/relay.json', relay_data)
